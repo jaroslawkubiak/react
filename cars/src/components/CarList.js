17dea6bb83a061ddbb27fa44e5d9f3436caf8c16
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-03-16 20:39:51 +0100

    finish app

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -4,11 +4,16 @@ import { removeCar } from "../store/slice/carsSlice";
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
     //filtering logic - search term
-    return data.filter(car => {
+    const filteredCars = data.filter(car => {
       return car.name.toLowerCase().includes(searchTerm.toLowerCase());
     });
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = car => {
@@ -16,8 +21,10 @@ function CarList() {
   };
 
   const renderedData = cars.map(car => {
+    // decide if the car should be bold font
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - {car.cost} zł
         </p>
